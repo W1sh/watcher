ff8afaa933116e753807cca971478a7103827372
Bruno Miguel Ferreira <bruno.miguel.ferreira@everis.com>
2019-11-07 16:31:29 +0000

    implementação da auth guard no angular

--- watcher-front-end/src/app/app-routing.module.ts
+++ watcher-front-end/src/app/app-routing.module.ts
@@ -4,16 +4,16 @@ import {HomeComponent} from './modules/home/home.component';
 import {MoviesComponent} from './modules/movies/movies.component';
 import {LoginComponent} from './modules/login/login.component';
 import {RegisterComponent} from './modules/register/register.component';
-import {DashboardComponent} from "./modules/dashboard/dashboard.component";
+import {DashboardComponent} from './modules/dashboard/dashboard.component';
+import {AuthGuard} from './core/guards/auth.guard';
 
 
 const routes: Routes = [
-  {path: '', component: LoginComponent},
-  {path: 'home', component: HomeComponent},
-  {path: 'movies', component: MoviesComponent},
+  {path: 'home', component: HomeComponent, canActivate: [AuthGuard]},
+  {path: 'movies', component: MoviesComponent, canActivate: [AuthGuard]},
   {path: 'login', component: LoginComponent},
   {path: 'register', component: RegisterComponent},
-  {path: 'dashboard', component: DashboardComponent}
+  {path: 'dashboard', component: DashboardComponent, canActivate: [AuthGuard]}
 ];
 
 @NgModule({
---
+++ watcher-front-end/src/app/core/guards/auth.guard.ts
@@ -0,0 +1,18 @@
+import { Injectable } from '@angular/core';
+import {CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree, Router} from '@angular/router';
+import { Observable } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AuthGuard implements CanActivate {
+
+  constructor(private router: Router) {}
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    //this.router.navigate(['/login']);
+    return true;
+  }
+
+}
