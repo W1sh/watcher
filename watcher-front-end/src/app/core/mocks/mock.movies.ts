b1815c72ac35335ab8141709fffbc0911dc6d35d
W1sh <brunoferreira.ebm@gmail.com>
2019-11-08 10:13:15 +0000

    implementação da filtragem no dashboard

--- watcher-front-end/src/app/core/mocks/mock.movies.ts
+++ watcher-front-end/src/app/core/mocks/mock.movies.ts
@@ -1,4 +1,4 @@
-import  { Movie } from '../../shared/models/movie';
+import { Movie } from '../../shared/models/movie';
 
 export const MOVIES: Movie[] = [
   {id: 1, original_title : 'abc', popularity: 2.00, release_date: '01-10-2019', vote_count: 7000, vote_average: 7},
--- watcher-front-end/src/app/modules/dashboard/dashboard.component.ts
+++ watcher-front-end/src/app/modules/dashboard/dashboard.component.ts
@@ -1,6 +1,10 @@
 import {Component, OnInit} from '@angular/core';
 import {Tile} from '../../shared/models/tile';
 import {FormBuilder, FormControl, Validators} from '@angular/forms';
+import {BehaviorSubject, combineLatest, Observable, of} from 'rxjs';
+import {filter, map, tap} from 'rxjs/operators';
+import {MOVIES} from '../../core/mocks/mock.movies';
+import {Movie} from '../../shared/models/movie';
 
 @Component({
   selector: 'app-dashboard',
@@ -8,7 +12,7 @@ import {FormBuilder, FormControl, Validators} from '@angular/forms';
   styleUrls: ['./dashboard.component.scss']
 })
 export class DashboardComponent implements OnInit {
-  tiles: Tile[] = [
+  /*tiles: Tile[] = [
     {text: 'One', cols: 1, rows: 1, color: 'lightblue'},
     {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
@@ -21,17 +25,35 @@ export class DashboardComponent implements OnInit {
     {text: 'Two', cols: 1, rows: 1, color: 'lightgreen'},
     {text: 'Three', cols: 1, rows: 1, color: 'lightpink'},
     {text: 'Four', cols: 1, rows: 1, color: '#DDBDF1'},
-  ];
+  ];*/
   search: string;
+  filteredSortedTiles: Observable<Tile[]>;
 
-  constructor() { }
+  constructor() {
+    this.filteredSortedTiles = of(this.moviesToTiles(MOVIES));
+    console.log(this.filteredSortedTiles);
+  }
 
   ngOnInit() {
 
   }
 
+  moviesToTiles(movies: Movie[]): Tile[] {
+    const movieTiles: Tile[] = [];
+    movies.forEach((movie) => {
+      movieTiles.push({text: movie.original_title, cols: 1, rows: 1, color: 'lightblue'});
+    });
+    return movieTiles;
+  }
+
   showDetails(tile: Tile) {
     console.log('ALLO');
+  }
+
+  filter() {
+    this.filteredSortedTiles = this.filteredSortedTiles.pipe(
+      map(tiles => tiles.filter(tile => tile.text.toLowerCase().includes(this.search)))
+    );
   }
 }
 
